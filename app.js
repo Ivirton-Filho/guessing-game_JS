1af903120005edd578a4397a39d6f307c29043b0
Ivirton-Filho <ivirtonfilho@gmail.com>
2025-04-22 15:55:32 -0300

    Resolvendo logica do loop Restart

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 window.alert("Boas vindas! ao jogo do número secreto!");
 let numeroSecreto = 19;
-let chute = prompt("Digite o número entre 1 e 30");
-do{
-    
+let chute = 0;
+let validarLoop = 0;
+do {
+    chute = prompt("Digite o número entre 1 e 30");
+
     if (chute == numeroSecreto) {
         window.alert(`Você acertou! O número secreto é ${numeroSecreto}`);
     } else {
         window.alert("Você errou!");
-    
+
         if (chute > numeroSecreto) {
             window.alert("O seu chute é MAIOR que o número secreto");
         } else {
             window.alert("O seu chute é MENOR que o número secreto ");
         }
     }
-    let validarLoop = 0;
     let replay = prompt("Você deseja jogar novamente? (S/N) ");
-        if(replay == N) {
-             validarLoop = 1;
-        } else{
-            continue;
-        }
-} while (validarLoop !== 1);
+    if (replay === "N") {
+        validarLoop = true;
+    }
+} while (!validarLoop);
