def9d0449385b7e23b38d1d39cff7712675f6a5c
Ivirton-Filho <ivirtonfilho@gmail.com>
2025-04-23 01:20:03 -0300

    Sistema de tentativas

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ window.alert("Boas vindas! ao jogo do número secreto!");
 let numeroSecreto = 19;
 let chute = 0;
 let validarLoop = 0;
+let tentativas = 1;
 do {
     chute = prompt("Digite o número entre 1 e 30");
-    if( chute < 0 || chute > 30){
+    if (chute < 0 || chute > 30) {
         window.alert("Digite um número valido");
         continue;
     }
 
     if (chute == numeroSecreto) {
-        window.alert(`Você acertou! O número secreto é ${numeroSecreto}`);
+        window.alert(`Você acertou! O número secreto é ${numeroSecreto} com ${tentativas} tentativas`);
     } else {
         window.alert("Você errou!");
 
@@ -19,6 +20,7 @@ do {
         } else {
             window.alert("O seu chute é MENOR que o número secreto ");
         }
+        tentativas ++;
     }
     let replay = prompt("Você deseja jogar novamente? (S/N) ");
     if (replay === "N") {
